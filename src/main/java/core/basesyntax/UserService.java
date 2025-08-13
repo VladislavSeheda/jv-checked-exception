46738635657728a5a1e424f683e7624b9dbc59b8
Владіслав <vladislav.seheda@gmail.com>
2025-08-13 13:29:26 +0300

    solve2

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,13 +2,15 @@ package core.basesyntax;
 
 public class UserService extends PasswordValidator {
 
-    public void registerUser(User user, String password, String repeatPassword)
+    public void registerUser(User user, String password,
+                             String repeatPassword)
             throws PasswordValidationException {
         if (!password.equals(repeatPassword)) {
             throw new PasswordValidationException("Passwords do not match");
         }
 
-        validate(password, repeatPassword);  // этот метод кидает checked exception, поэтому throws надо здесь
+        validate(password, repeatPassword);
+        // этот метод кидает checked exception, поэтому throws надо здесь
         saveUser(user);
     }
 
