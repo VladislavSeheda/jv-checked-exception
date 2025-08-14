83e8784273083984a6212783ddadd8519c950660
Владіслав <vladislav.seheda@gmail.com>
2025-08-14 12:18:24 +0300

    done task

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,13 +3,15 @@ package core.basesyntax;
 public class PasswordValidator {
     private static final int MIN_PASSWORD_LENGTH = 10;
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (!password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
 
         if (password.length() < MIN_PASSWORD_LENGTH) {
-            throw new PasswordValidationException("Password length is less than " + MIN_PASSWORD_LENGTH);
+            throw new PasswordValidationException("Password length is less than "
+                    + MIN_PASSWORD_LENGTH);
         }
     }
 }
